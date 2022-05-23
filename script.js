713d9fdd3960e0f4cf1e592f1d5fda19c493938d
rlw92 <r_l_williams@yahoo.com>
2022-05-23 14:41:22 +0100

    Testing the choose coordinates function, I would like it so that the players can choose their ships using the grid layout and am buildiing thee functionality of that.

--- script.js
+++ script.js
@@ -19,17 +19,22 @@ if(id===0 && ltr==="0"){grid.textContent = no}
 else if(no===0 && ltr!="0"){grid.textContent = ltr}
 grid.dataset.hit = "No"
 let grdata = grid.dataset.hit;
+let letter = grid.dataset.letter = ltr;
+let number = grid.dataset.number = no;
 
 grid.addEventListener("mouseover",()=>{grid.style.backgroundColor = "black";})
 grid.addEventListener("mouseleave",()=>{if(grdata === "No"){grid.style.backgroundColor = "white"}else{grid.style.backgroundColor = "red"};})
-grid.addEventListener("click", ()=>{grdata = "hit"; grid.style.backgroundColor = "red";})
+grid.addEventListener("click", ()=>{grdata = "hit"; grid.style.backgroundColor = "red";console.log(letter);console.log(number);})
 let gameboard =  document.getElementById("gameboard");
 gameboard.appendChild(grid);
 }
 
 //create the whole grid with below function
 const createGameboard = () =>{
+
 for(i=0;i<11;i++){
+    
+
     GameboardModule.createGrids("0",0,i)
     GameboardModule.createGrids("A",i);
     GameboardModule.createGrids("B",i);
@@ -41,9 +46,27 @@ for(i=0;i<11;i++){
     GameboardModule.createGrids("H",i);
     GameboardModule.createGrids("I",i);
     GameboardModule.createGrids("J",i);
+
 }
 }
 return {createGrids,createGameboard}
 })();
 
 GameboardModule.createGameboard();
+
+
+
+function chooseCrdnts(StrtNo,StrtLttrs){
+    let coord = [];
+
+    //if(VorH==="Vertical"){}
+    for(let i=StrtNo;i<4;i++){
+        coord.push(StrtLttrs + StrtNo++)}
+
+    return coord
+
+}
+
+console.log(chooseCrdnts(1,"A"));
+
+
