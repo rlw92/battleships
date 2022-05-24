bd0b1ad8de370b5ae220d04622a64d6509fb017d
rlw92 <r_l_williams@yahoo.com>
2022-05-24 19:46:22 +0100

    Pushing because I have managed to add the functionality to allow player one and two to choose their coordinates, code is still a mess and nowhere near finished, but can progress and build from here, think I will implement webpack going forward as this could get messy.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+
+
 const GameboardModule = (() => {
 
 let bord = "solid black 2px";
 let nobord = "none";
+
 
 const createGrids = (ltr,no,id) =>{
 let grid = document.createElement("div")
@@ -11,7 +14,6 @@ if(id=== 0){grid.style.border=nobord}
 else if(no===0){grid.style.border = nobord;}
 else{grid.style.border = bord;}
 
-grid.setAttribute("id",ltr+ no)
 
 grid.textContent = id;
 grid.style.textAlign = "center";
@@ -21,12 +23,19 @@ grid.dataset.hit = "No"
 let grdata = grid.dataset.hit;
 let letter = grid.dataset.letter = ltr;
 let number = grid.dataset.number = no;
+//shows which player owns the cell//
+//let player = grid.dataset.playercell = "player"+plyr;
+
+//creating a class for each grid so we can access it from the functions
+grid.classList.add("cell");
 
 grid.addEventListener("mouseover",()=>{grid.style.backgroundColor = "black";})
 grid.addEventListener("mouseleave",()=>{if(grdata === "No"){grid.style.backgroundColor = "white"}else{grid.style.backgroundColor = "red"};})
-grid.addEventListener("click", ()=>{grdata = "hit"; grid.style.backgroundColor = "red";console.log(letter);console.log(number);})
-let gameboard =  document.getElementById("gameboard");
+                                                                   
+let gameboard =  document.getElementById("gameboard1");
+
 gameboard.appendChild(grid);
+
 }
 
 //create the whole grid with below function
@@ -52,21 +61,72 @@ for(i=0;i<11;i++){
 return {createGrids,createGameboard}
 })();
 
-GameboardModule.createGameboard();
 
 
+//below needs to be sorted into modules of their own at the moment
 
-function chooseCrdnts(StrtNo,StrtLttrs){
+function chooseCrdnts(StrtLttrs,StrtNo,length){
     let coord = [];
-
-    //if(VorH==="Vertical"){}
-    for(let i=StrtNo;i<4;i++){
-        coord.push(StrtLttrs + StrtNo++)}
-
-    return coord
+    let over = 11-length;
+    //building it so when the user selects a number it keeps it within the grid
+    if(StrtNo>over){StrtNo = over}
+    //if(VorH==="Vertical"){} Will be treating this testing as complete vertical positioning
+    for(let i=1;i<length+1;i++){
+        coord.push(StrtLttrs + StrtNo++)
+    }
+    return coord;
 
 }
 
-console.log(chooseCrdnts(1,"A"));
+let player1coord=[];
+let player2coord=[];
+
+//basically a factory function for a 5 square ship
+function bttlship1(t){
+t.target.style.backgroundColor = "red";
+let letter = t.target.dataset.letter;
+let number = t.target.dataset.number;
+player1coord.push(chooseCrdnts(letter,number,5));
+}
+
+//to be able to invoke both functions and to remove it
+function chan(t){
+    bttlship1(t);
+    pb2();
+}
+
+//adds an event listener to every grid cell allowing us to choose our coordinates,map them to the array then change player
+function pb1(){
+GameboardModule.createGameboard();
+let cells = document.querySelectorAll(".cell")
+for(i=0;i<cells.length;i++){
+    cells[i].addEventListener('click',chan)
+     
+}
+}
+
+function pb2(){
+    let cells = document.querySelectorAll(".cell")
+
+for(i=0;i<cells.length;i++){
+    cells[i].removeEventListener('click',chan)
+
+    cells[i].addEventListener('click',(t)=>{
+     t.target.style.backgroundColor = "red";
+     let letter = t.target.dataset.letter;
+    let number = t.target.dataset.number;
+     player2coord.push(chooseCrdnts(letter,number,5));
+     
+     
+})
+}
+}
+
+pb1();
+
+
+
+
+
 
 
