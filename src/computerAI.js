a87481badf7a315e2fe5253dbead2b23c494a528
rlw92 <r_l_williams@yahoo.com>
2022-06-17 15:37:44 +0100

    Wrote a brief idea for the computer AI, will begin work when possible.

---
+++ src/computerAI.js
@@ -0,0 +1,12 @@
+
+
+
+/* 
+The computer AI needs to be able to randomly select its' ship positions.
+Randomly select its' hit points.
+
+A more advanced AI would then be able to hit adjacent grids when it detects a hit.
+*/
+
+
+//We need the computer to generate its coordinates randomly then start the game
